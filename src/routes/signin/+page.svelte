570142e1a9bbb57b1f012c69b37426c353a0885b
rugeby <rubyleemkk@gmail.com>
2023-10-12 18:52:13 -0600

    update => work on database

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,140 +1,157 @@
+
 <script lang="ts">
-  import {goto} from '$app/navigation'
+  let email = '';
+  let password = '';
+  let repeatPassword = '';
+  let name = '';
+  let age = '';
+  let about = '';
 
+  function handleSubmit() {
+    // Here, you can send the form data to your backend or database using an HTTP request.
+    // Example:
+    const formData = {
+      email,
+      password,
+      name,
+      age: parseInt(age), // Convert age to a number
+      about,
+    };
 
-
+    // Send the formData to your backend for storage
+    fetch('/your-api-endpoint', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then(response => {
+        if (response.ok) {
+          // Handle success, e.g., show a success message
+          console.log('Data saved successfully.');
+        } else {
+          // Handle errors, e.g., display an error message
+          console.error('Error saving data.');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
 </script>
 
-<div>
-    <h1>Sign Up</h1>
-       <p>Please fill in this form to create an account.</p>
-      <hr>
-      <label for="email"><b>Email</b></label>
-      <input type="text" placeholder="Enter Email" name="email" required>
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
-      <label for="psw-repeat"><b>Repeat Password</b></label>
-      <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-    
+
+  
+  <form on:submit={handleSubmit}>
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" id="email" bind:value={email} required />
+    </div>
+
+    <div class="form-group">
+      <label for="email">Password: </label>
+      <input type="password" id="password" bind:value={password} required />
+    </div>
+
+    <div class="form-group">
+      <label for="email">Repeat password: </label>
+      <input type="password" id="repeatpassword" bind:value={repeatPassword} required />
+    </div>
 
 
-      <div class="clearfix">
-        <button type="button" class="cancelbtn">Cancel</button>
-        <button type="submit" on:click = {() => goto('/userInfoForm')} class="signupbtn">Sign Up</button>
-      </div>
-</div>
+    <div class="form-group">
+      <label for="name">Name:</label>
+      <input type="text" id="name" bind:value={name} required />
+    </div>
+  
+  
+    <div class="form-group">
+      <label for="age">Age:</label>
+      <input type="number" id="age" bind:value={age} required />
+    </div>
+  
+    <div class="form-group">
+      <label for="about">Tell Us About Yourself!</label>
+      <textarea id="about" bind:value={about} rows="4"></textarea>
+    </div>
+  
+    <button type="submit">Submit</button>
+    <button class="bg-neutral-600 text-white px-2 py-1 rounded-[4px] m-1">Back</button>
 
-<style>
-    body {font-family: Arial,
-    Helvetica, sans-serif;}
-* {box-sizing: border-box;}
-
-/* Full-width input fields */
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  display: inline-block;
-  border: none;
-  background: #f1f1f1;
-}
-
-/* Add a background color when the inputs get focus */
-input[type=text]:focus, input[type=password]:focus {
-  background-color: #ddd;
-  outline: none;
-}
-
-/* Set a style for all buttons */
-button {
-  background-color: #04AA6D;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-}
-
-button:hover {
-  opacity:1;
-}
-
-/* Extra styles for the cancel button */
-.cancelbtn {
-  padding: 14px 20px;
-  background-color: #f44336;
-}
-
-/* Float cancel and signup buttons and add an equal width */
-.cancelbtn, .signupbtn {
-  float: left;
-  width: 50%;
-}
-
-/* Add padding to container elements */
-.container {
-  padding: 16px;
-}
-
-/* The Modal (background) */
-.modal {
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: #474e5d;
-  padding-top: 50px;
-}
-
-/* Modal Content/Box */
-.modal-content {
-  background-color: #fefefe;
-  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
-  border: 1px solid #888;
-  width: 80%; /* Could be more or less, depending on screen size */
-}
-
-/* Style the horizontal ruler */
-hr {
-  border: 1px solid #f1f1f1;
-  margin-bottom: 25px;
-}
- 
-/* The Close Button (x) */
-.close {
-  position: absolute;
-  right: 35px;
-  top: 15px;
-  font-size: 40px;
-  font-weight: bold;
-  color: #f1f1f1;
-}
-
-.close:hover,
-.close:focus {
-  color: #36d1f4;
-  cursor: pointer;
-}
-
-/* Clear floats */
-.clearfix::after {
-  content: "";
-  clear: both;
-  display: table;
-}
-
-/* Change styles for cancel button and signup button on extra small screens */
-@media screen and (max-width: 300px) {
-  .cancelbtn, .signupbtn {
-     width: 100%;
+  </form>
+  
+  <style>
+    /* Add CSS styles for the form and its elements */
+    h1.center {
+    font-size: 24px;
+    margin-bottom: 20px;
+    text-align: center; /* Center the text horizontally */
   }
-}
+  
+    form {
+      max-width: 400px;
+      margin: 0 auto;
+      padding: 20px;
+      background-color: #f5f5f5;
+      border: 1px solid #ddd;
+      border-radius: 5px;
+      display: flex;
+      flex-direction: column;
+    }
+  
+    .form-group {
+      display: flex;
+      flex-direction: column; /* Stack label and input vertically */
+      margin-bottom: 20px; /* Increase spacing between groups */
+    }
+  
+    label {
+      font-weight: bold;
+      text-align: left; /* Align labels to the left */
+      margin-bottom: 5px; /* Add space below labels */
+    }
+  
+    input[type="text"],
+    input[type="email"],
+    input[type="number"],
+    input[type="password"],
+    input[type="repeatpassword"],
 
-</style>
-
+    textarea {
+      width: 100%;
+      padding: 10px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      font-size: 16px;
+    }
+  
+    /* Customize the appearance of the Age input */
+    input[type="number"] {
+      width: 50px; /* Adjust the width to make it smaller */
+      padding: 8px;
+      text-align: center; /* Center the text in the input */
+      font-weight: bold; /* Make the text bold */
+      font-size: 18px; /* Adjust the font size */
+    }
+  
+    textarea {
+      height: 100px;
+    }
+  
+    button {
+      background-color: rgb(92, 122, 154);
+      color: #fff;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 5px;
+      font-size: 18px;
+      cursor: pointer;
+      align-self: flex-start; /* Align the button to the left */
+    }
+  
+    button:hover {
+      background-color: #5890df;
+    }
+  </style>
+  
