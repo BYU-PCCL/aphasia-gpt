8387a4482b317e15d3636d084808a7c89103d0ab
rugeby <rubyleemkk@gmail.com>
2023-10-13 14:23:33 -0600

    database in progress...

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,13 @@
 import type { Handle } from "@sveltejs/kit";
+import { start_mongo } from "./db/mongo";
+
 
 export const handle: Handle = async ({ event, resolve }) => {
   event.locals.username = event.cookies.get("username") ?? null;
   const response = await resolve(event);
   return response;
 };
+
+start_mongo().then(():void=>{
+  console.log("Mongo started ");
+}). catch(e => {console.error(e)})
