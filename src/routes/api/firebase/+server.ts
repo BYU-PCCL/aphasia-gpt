9c3aa6a4945bcd892cc4a723c0b817c60365ac55
porcupineparty <matlanet@gmail.com>
2023-11-16 16:34:56 -0700

    upToDate

--- src/routes/api/firebase/+server.ts
+++ src/routes/api/firebase/+server.ts
@@ -1,6 +1,7 @@
+
 // Import the functions you need from the SDKs you need
 if (!import.meta.env.SSR) {
-  throw new Error("This module can only be imported on the server side.");
+throw new Error("This module can only be imported on the server side.");
 }
 import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
@@ -9,66 +10,64 @@ import admin from "firebase-admin";
 import serviceAccount from "../../../../admin.config/admin.json";
 
 if(!admin.apps.length){
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-    databaseURL: "https://brocas-userdb-default-rtdb.firebaseio.com"
-  });
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+databaseURL: "https://brocas-userdb-default-rtdb.firebaseio.com"
+});
 }
 
 // const loginEmailPassword = async()=>{
-//   const loginEmail = email;
-//   const loginPassword = password;
+// const loginEmail = email;
+// const loginPassword = password;
 
 // }
 
 function writeUserData(username: string, email: string, password: string, name: string, age: string, about: string){
-  const db = admin.database();
-  const ref = db.ref('users');
-  const usersRef = ref.child(username);
-  
+const db = admin.database();
+const ref = db.ref('users');
+const usersRef = ref.child(username);
 
-  console.log("this is a test for write user data")
-  const userData = {
-    email: email,
-    password: password,
-    name: name,
-    age: age,
-    about: about
-  }
-  usersRef.set(userData);
+console.log("this is a test for write user data")
+const userData = {
+email: email,
+password: password,
+name: name,
+age: age,
+about: about
+}
+usersRef.set(userData);
 }
 
 
 export const GET: RequestHandler = async () => {
-    console.log("this is a test");
-    return json(
-      {
-        status: 200,
-      }
-    );
-  };
+console.log("this is a test");
+return json(
+{
+status: 200,
+}
+);
+};
 
 export const POST: RequestHandler = async ({ request }) => {
-    console.log("fire base is called");
-    try {
-       const {signinusername, email, password, name, age, about} = await request.json();
-       console.log('username from frontend:', signinusername);
-        console.log('email from frontend:', email);
-        console.log('password from frontend:', password);
-        console.log('name from frontend:', name);
-        console.log('age from frontend:', age);
-        console.log('about from frontend:', about);
-        writeUserData(signinusername,email, password, name, age, about)
-      return json({
-        status: 200,
-        body: { message: 'Data received successfully.' },
-      });
-    } catch (error) {
-      console.error('Error processing request:', error);
-      return json({
-        status: 500,
-        body: { error: 'hi Server Error' },
-      });
-    }
-  };
-  
+console.log("fire base is called");
+try {
+const {signinusername, email, password, name, age, about} = await request.json();
+console.log('username from frontend:', signinusername);
+console.log('email from frontend:', email);
+console.log('password from frontend:', password);
+console.log('name from frontend:', name);
+console.log('age from frontend:', age);
+console.log('about from frontend:', about);
+writeUserData(signinusername,email, password, name, age, about)
+return json({
+status: 200,
+body: { message: 'Data received successfully.' },
+});
+} catch (error) {
+console.error('Error processing request:', error);
+return json({
+status: 500,
+body: { error: 'hi Server Error' },
+});
+}
+};
--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,215 +1,188 @@
 
+
 <script lang="ts">
-  import {goto} from '$app/navigation'
-  import { json } from "@sveltejs/kit";
-  import { username } from "@/stores/user";
+import {goto} from '$app/navigation'
+import { json } from "@sveltejs/kit";
+import { username } from "@/stores/user";
+export{signinusername}
 
-  let signinusername = '';
-  let email = '';
-  let password = '';
-  let repeatPassword = '';
-  let name = '';
-  let age = '';
-  let about = '';
-  let passwordMatch = true;
-  let errorMessage = '';
+let signinusername = '';
+let email = '';
+let password = '';
+let repeatPassword = '';
+let name = '';
+let age = '';
+let about = '';
+let passwordMatch = true;
+let errorMessage = '';
 
 
-  function checkpassword(password, repeatPassword) {
-  passwordMatch = password === repeatPassword;
-  errorMessage = passwordMatch ? '' : 'Passwords do not match';
+function checkpassword(password, repeatPassword) {
+passwordMatch = password === repeatPassword;
+errorMessage = passwordMatch ? '' : 'Passwords do not match';
 }
 
 
 
-  function handleSubmit() {
-    checkpassword(password, repeatPassword);
-    if (!passwordMatch) {
-    // Display an error message or handle the validation as needed
-    console.error(errorMessage);
-    return;
-  }
-
-    $username = signinusername
-    
-    // Here, you can send the form data to your backend or database using an HTTP request.
-    // Example:
-    // const formData = {
-    //   signinusername,
-    //   email,
-    //   password,
-    //   name,
-    //   age: parseInt(age), // Convert age to a number
-    //   about,
-    // };
-   sendDataToBackend(signinusername, email, password, name, age, about)
-  }
+function handleSubmit() {
+checkpassword(password, repeatPassword);
+if (!passwordMatch) {
+// Display an error message or handle the validation as needed
+console.error(errorMessage);
+return;
+}
+sendDataToBackend(signinusername, email, password, name, age, about)
+$username = signinusername
+}
 
 
-  async function sendDataToBackend(signinusername, email, password, name, age, about){
-    try{
-  
-      const response = await fetch("/api/firebase", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({signinusername, email, password, name, age, about}),
-      });
-      
-      console.log(password);
-      console.log('Full response from server:', response);
+async function sendDataToBackend(signinusername, email, password, name, age, about){
+try{
+const response = await fetch("/api/firebase", {
+method: 'POST',
+headers: {
+'Content-Type': 'application/json',
+},
+body: JSON.stringify({signinusername, email, password, name, age, about}),
+});
 
-      
-      if(!response.ok){
-        const errorData = await response.json();
-        console.error('Error data from server:', errorData);
-        throw new Error('Network response was not ok');
-      }
+console.log('Full response from server:', response);
 
-      const data = await response.json();
-      console.log('Response from backend:', data);
-    }catch (error){
-      console.error('Error:', error);
-    }
-  }
+if(!response.ok){
+const errorData = await response.json();
+console.error('Error data from server:', errorData);
+throw new Error('Network response was not ok');
+}
+
+const data = await response.json();
+console.log('Response from backend:', data);
+}catch (error){
+console.error('Error:', error);
+}
+}
 </script>
 
 
-  
-  <form on:submit={handleSubmit}>
-    <div class="form-group">
-      <label for="username">Username:</label>
-      <input type="username" id="username" placeholder="Do Not include any special character" bind:value={signinusername} required />
-    </div>
+<form on:submit={handleSubmit}>
+<div class="form-group">
+<label for="username">Username:</label>
+<input type="username" id="username" placeholder="Do Not include any special character" bind:value={signinusername} required />
+</div>
 
-    <div class="form-group">
-      <label for="email">Email:</label>
-      <input type="email" id="email" placeholder="Grand@email.com" bind:value={email} required />
-    </div>
+<div class="form-group">
+<label for="email">Email:</label>
+<input type="email" id="email" placeholder="Grand@email.com" bind:value={email} required />
+</div>
 
-    <div class="form-group">
-      <label for="password">Password: </label>
-      <input
-        type="password"
-        id="password"
-        placeholder="Password"
-        bind:value={password}
-        required
-        on:input={() => checkpassword(password, repeatPassword)}
-      />
-    </div>
+<div class="form-group">
+<label for="password">Password: </label>
+<input
+type="password"
+id="password"
+placeholder="Password"
+bind:value={password}
+required
+on:input={() => checkpassword(password, repeatPassword)}
+/>
+</div>
 
-    <div class="form-group">
-      <label for="repeatpassword">Repeat password: </label>
-      <input
-        type="password"
-        id="repeatpassword"
-        placeholder="Confirm Password"
-        bind:value={repeatPassword}
-        required
-        on:input={() => checkpassword(password, repeatPassword)}
-      />
-      {#if !passwordMatch}
-        <p class="error-message">{errorMessage}</p>
-      {/if}
-    </div>
-
-
-    <div class="form-group">
-      <label for="name">Name:</label>
-      <input type="text" id="name" bind:value={name} required />
-    </div>
-  
-  
-    <div class="form-group">
-      <label for="age">Age:</label>
-      <input type="number" id="age" bind:value={age} required />
-    </div>
-  
-    <div class="form-group">
-      <label for="about">Tell Us About Yourself!</label>
-      <textarea id="about" bind:value={about} rows="4"></textarea>
-    </div>
-  
-    <button  on:click={handleSubmit}  on:click={() => goto('/')} type="button" style="display: inline-block;" >Submit</button>
-    <button on:click={() => goto('/')} class="bg-neutral-600 text-white px-2 py-1 rounded-[4px] m-1" style="display: inline-block;">Back</button>
+<div class="form-group">
+<label for="repeatpassword">Repeat password: </label>
+<input
+type="password"
+id="repeatpassword"
+placeholder="Confirm Password"
+bind:value={repeatPassword}
+required
+on:input={() => checkpassword(password, repeatPassword)}
+/>
+{#if !passwordMatch}
+<p class="error-message">{errorMessage}</p>
+{/if}
+</div>
 
 
-  </form>
-  
-  <style>
-    /* Add CSS styles for the form and its elements */
-    h1.center {
-    font-size: 24px;
-    margin-bottom: 20px;
-    text-align: center; /* Center the text horizontally */
-  }
-  
-    form {
-      max-width: 400px;
-      margin: 0 auto;
-      padding: 20px;
-      background-color: #f5f5f5;
-      border: 1px solid #ddd;
-      border-radius: 5px;
-      display: flex;
-      flex-direction: column;
-    }
-  
-    .form-group {
-      display: flex;
-      flex-direction: column; /* Stack label and input vertically */
-      margin-bottom: 20px; /* Increase spacing between groups */
-    }
-  
-    label {
-      font-weight: bold;
-      text-align: left; /* Align labels to the left */
-      margin-bottom: 5px; /* Add space below labels */
-    }
+<div class="form-group">
+<label for="name">Name:</label>
+<input type="text" id="name" bind:value={name} required />
+</div>
+<div class="form-group">
+<label for="age">Age:</label>
+<input type="number" id="age" bind:value={age} required />
+</div>
+<div class="form-group">
+<label for="about">Tell Us About Yourself!</label>
+<textarea id="about" bind:value={about} rows="4"></textarea>
+</div>
+<button on:click={handleSubmit} on:click={() => goto('/')} type="button" style="display: inline-block;" >Submit</button>
+<button on:click={() => goto('/')} class="bg-neutral-600 text-white px-2 py-1 rounded-[4px] m-1" style="display: inline-block;">Back</button>
 
-    input[type="username"],
-    input[type="text"],
-    input[type="email"],
-    input[type="number"],
-    input[type="password"],
-    input[type="repeatpassword"],
 
-    textarea {
-      width: 100%;
-      padding: 10px;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-      font-size: 16px;
-    }
-  
-    /* Customize the appearance of the Age input */
-    input[type="number"] {
-      width: 50px; /* Adjust the width to make it smaller */
-      padding: 8px;
-      text-align: center; /* Center the text in the input */
-      font-weight: bold; /* Make the text bold */
-      font-size: 18px; /* Adjust the font size */
-    }
-  
-    textarea {
-      height: 100px;
-    }
-  
-    button {
-      background-color: rgb(92, 122, 154);
-      color: #fff;
-      padding: 10px 20px;
-      border: none;
-      border-radius: 5px;
-      font-size: 18px;
-      cursor: pointer;
-      align-self: flex-start; /* Align the button to the left */
-    }
-  
-    button:hover {
-      background-color: #5890df;
-    }
-  </style>
-  
+</form>
+<style>
+/* Add CSS styles for the form and its elements */
+h1.center {
+font-size: 24px;
+margin-bottom: 20px;
+text-align: center; /* Center the text horizontally */
+}
+form {
+max-width: 400px;
+margin: 0 auto;
+padding: 20px;
+background-color: #f5f5f5;
+border: 1px solid #ddd;
+border-radius: 5px;
+display: flex;
+flex-direction: column;
+}
+.form-group {
+display: flex;
+flex-direction: column; /* Stack label and input vertically */
+margin-bottom: 20px; /* Increase spacing between groups */
+}
+label {
+font-weight: bold;
+text-align: left; /* Align labels to the left */
+margin-bottom: 5px; /* Add space below labels */
+}
+
+input[type="username"],
+input[type="text"],
+input[type="email"],
+input[type="number"],
+input[type="password"],
+input[type="repeatpassword"],
+
+textarea {
+width: 100%;
+padding: 10px;
+border: 1px solid #ccc;
+border-radius: 5px;
+font-size: 16px;
+}
+/* Customize the appearance of the Age input */
+input[type="number"] {
+width: 50px; /* Adjust the width to make it smaller */
+padding: 8px;
+text-align: center; /* Center the text in the input */
+font-weight: bold; /* Make the text bold */
+font-size: 18px; /* Adjust the font size */
+}
+textarea {
+height: 100px;
+}
+button {
+background-color: rgb(92, 122, 154);
+color: #fff;
+padding: 10px 20px;
+border: none;
+border-radius: 5px;
+font-size: 18px;
+cursor: pointer;
+align-self: flex-start; /* Align the button to the left */
+}
+button:hover {
+background-color: #5890df;
+}
+</style>
