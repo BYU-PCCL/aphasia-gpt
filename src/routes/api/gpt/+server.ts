76c13ca741e0f2fb2f11e487c1b82782c30fe222
Spencer M <spencer.g.moffat@gmail.com>
2024-01-22 19:26:10 -0700

    Add TonePicker component and implement basic tone selection
    Very simple, no styling yet.
    Stores selected tone on transcriptProcessor state.
    Modifies GPT prompt to interpet in selected tone of voice.

--- src/routes/api/gpt/+server.ts
+++ src/routes/api/gpt/+server.ts
@@ -1,8 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 import { OPENAI_API_KEY } from "$env/static/private";
 import { json, type RequestHandler } from "@sveltejs/kit";
-import invariant from "tiny-invariant";
-import { onDestroy } from 'svelte';
 
 const configuration = new Configuration({
   apiKey: OPENAI_API_KEY,
@@ -13,9 +11,10 @@ const model = 'gpt-3.5-turbo-instruct'; // Use the GPT-3 model
 let value = "Broca's Aphasia";
 
 export const POST: RequestHandler = async ({ request }) => {
-  const { aphasiaType, utterance } = await request.json();
+  const { aphasiaType, utterance, tone } = await request.json();
   console.log('received aphasia type from frontend:', aphasiaType);
   console.log("received utterance from frontend:", utterance);
+  console.log("received tone from frontend:", tone);
 
   if (aphasiaType) {
     value = aphasiaType;
@@ -37,7 +36,7 @@ export const POST: RequestHandler = async ({ request }) => {
           5. "cat seems cat" => "The cat seems hungry"
           6. "i i need i need some" => "I need to get some sleep"
           
-          Now, please provide three transformed and predicted sentences (separated by line break) for the following utterance: 
+          Now, please provide three transformed and predicted sentences (separated by line break) for the following utterance in a ${tone} tone of voice: 
           ${utterance}`,
       temperature: .5,
       max_tokens: 400, // Set the desired maximum token length for the response
